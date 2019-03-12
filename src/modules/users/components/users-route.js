35a61aaa40d94a8d21558980d9688b74a91cebc6
Stepan Sojka <stepan@salsitasoft.com>
2019-03-12 14:09:45 +0100

    adding forgotten files

---
+++ src/modules/users/components/users-route.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { RouterSelectors } from "@salsita/react-router";
+
+import { USER_DETAIL } from "router/routes";
+import { UserList } from "modules/users/components/users-list";
+import { UserDetail } from "modules/users/components/user-detail";
+
+const DumbUsersRoute = ({ route }) => {
+  if (route && route.name === USER_DETAIL.name) {
+    return <UserDetail />;
+  }
+
+  return <UserList />;
+};
+
+const mapStateToProps = state => ({
+  route: RouterSelectors.getCurrentRoute(state)
+});
+
+export const UsersRoute = connect(mapStateToProps)(DumbUsersRoute);
---
+++ src/router/routes.js
@@ -0,0 +1,2 @@
+export const USER_LIST = { name: "users", path: "/users" };
+export const USER_DETAIL = { name: "users.detail", path: "/detail" };
