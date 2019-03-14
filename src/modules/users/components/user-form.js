a1aacceb85fcbc0f88ce5522d0ddd55e527c390c
Stepan Sojka <stepan@salsitasoft.com>
2019-03-14 14:43:58 +0100

    trying to use inital values

--- src/modules/users/components/user-form.js
+++ src/modules/users/components/user-form.js
@@ -3,14 +3,23 @@ import { reduxForm } from "redux-form";
 import { FormField } from "@salsita/react-forms";
 import { FormFieldSelect } from "@salsita/react-forms";
 
+import { getCurrentUser } from "modules/users/user-selectors";
+
 const UserFormInternal = ({ handleSubmit }) => (
   <form onSubmit={handleSubmit}>
     <FormField type="text" name="firstName" label="first name" />
     <FormField type="text" name="lastName" label="last name" />
     <FormFieldSelect name="skills" />
 
-    <button type="submit">Create</button>
+    <button type="submit">Save</button>
   </form>
 );
 
-export const UserForm = reduxForm({ form: "user" })(UserFormInternal);
+const mapStateToProps = state => ({
+  initialValues: getCurrentUser(state),
+  enableReinitialize: true
+});
+
+export const UserForm = reduxForm({ form: "user" }, mapStateToProps)(
+  UserFormInternal
+);
