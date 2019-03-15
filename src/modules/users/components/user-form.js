97f02ccebd53de670f89bcf1da0f34f5b2ca9632
Stepan Sojka <stepan@salsitasoft.com>
2019-03-15 09:15:19 +0100

    added form validations

--- src/modules/users/components/user-form.js
+++ src/modules/users/components/user-form.js
@@ -2,15 +2,32 @@ import React from "react";
 import { reduxForm } from "redux-form";
 import { FormField } from "@salsita/react-forms";
 import { FormFieldSelect } from "@salsita/react-forms";
+import { FormValidations } from "@salsita/react-forms";
 
 import { getCurrentUser } from "modules/users/user-selectors";
 
+const validateFirstName = FormValidations.notEmptyString(
+  "first name cannot be empty"
+);
+const validateLastName = FormValidations.notEmptyString(
+  "first name cannot be empty"
+);
+
 const UserFormInternal = ({ handleSubmit }) => (
   <form onSubmit={handleSubmit}>
-    <FormField type="text" name="firstName" label="first name" />
-    <FormField type="text" name="lastName" label="last name" />
+    <FormField
+      type="text"
+      name="firstName"
+      validate={validateFirstName}
+      label="first name"
+    />
+    <FormField
+      type="text"
+      name="lastName"
+      validate={validateLastName}
+      label="last name"
+    />
     <FormFieldSelect name="skills" />
-
     <button type="submit">Save</button>
   </form>
 );
