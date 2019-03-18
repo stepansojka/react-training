eb9e0f050cfcee36946d993f0bdbcbce4e588437
Stepan Sojka <stepan@salsitasoft.com>
2019-03-18 12:39:52 +0100

    adding forgotten file

---
+++ src/modules/users/components/user-update.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { UserUpdateForm } from "modules/users/components/user-form";
+import { UserActionCreators } from "modules/users/user-actions";
+import { getCurrentUser } from "modules/users/user-selectors";
+
+const DumbUserUpdate = ({ currentUser, saveUser }) => (
+  <UserUpdateForm
+    initialValues={currentUser}
+    onSubmit={thing => console.log("in form:", thing) || saveUser(thing)}
+  />
+);
+
+const mapStateToProps = state => ({
+  currentUser: getCurrentUser(state)
+});
+
+const mapDispatchToProps = {
+  saveUser: UserActionCreators.saveUser
+};
+
+export const UserUpdate = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DumbUserUpdate);
