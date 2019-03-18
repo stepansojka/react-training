ea427f03a937a4561cab7953b2bcb10cf8ff910d
Stepan Sojka <stepan@salsitasoft.com>
2019-03-18 12:52:47 +0100

    show skill lables when updating user

--- src/modules/users/user-selectors.js
+++ src/modules/users/user-selectors.js
@@ -8,19 +8,30 @@ export const getTitle = state => state.header.title;
 
 const maybe = fn => arg => (arg ? fn(arg) : arg);
 
+const enrichSkills = maybe(skills =>
+  skills.map(skill => ({
+    ...skill,
+    label: skill.name
+  }))
+);
+
+const enrichUserSkills = maybe(skills =>
+  skills.map(skill => ({
+    ...skill,
+    label: skill.skill.name
+  }))
+);
+
 const enrichUser = maybe(user => ({
   ...user,
   lastName: user.lastName.toUpperCase(),
-  regnalNumber: toRoman(user.regnalNumber)
+  regnalNumber: toRoman(user.regnalNumber),
+  skills: enrichUserSkills(user.skills)
 }));
 
 export const getSkillList = createSelector(
   getSkills,
-  skills =>
-    skills.map(skill => ({
-      ...skill,
-      label: skill.name
-    }))
+  skills => enrichSkills(skills)
 );
 
 export const getUserList = createSelector(
