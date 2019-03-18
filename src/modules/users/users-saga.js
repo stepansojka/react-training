3db96739cd6c8ce74e4d32f1c8b14641a976bb65
Stepan Sojka <stepan@salsitasoft.com>
2019-03-18 14:12:01 +0100

    add router transition

--- src/modules/users/users-saga.js
+++ src/modules/users/users-saga.js
@@ -1,7 +1,10 @@
-import { call, fork, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from "redux-saga/effects";
 import { saveEntity, fetchEntities } from "@salsita/react-crud";
+import { RouterActions } from "@salsita/react-router";
 
 import { UserActionTypes } from "modules/users/user-actions";
+
+import { USER_LIST as USER_LIST_ROUTE } from "modules/router/routes";
 
 import { USER, USER_LIST } from "modules/crud/crud-entities";
 import {
@@ -12,7 +15,8 @@ import {
 export function* saveUser({ user }) {
   try {
     yield call(saveEntity, user, USER, mapEntityToSaveParams);
-    yield fork(fetchUsers);
+    yield call(fetchUsers);
+    yield put(RouterActions.Creators.navigateTo(USER_LIST_ROUTE.NAME));
   } catch (e) {
     console.error(e);
   }
